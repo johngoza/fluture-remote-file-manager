db85507737d96ed72d806db61a138db15a7ff8b8
Jack Goza <jgoza@q6inc.com>
2020-05-11 11:28:32 -0500

    add more tests for ftp sendfile method add tests for ftp sendfile method to ensure future is returned, validate returned "error" object add commentary from chat with Michael regarding Sanctuary

--- tests/unit/lib/ftp-tests.js
+++ tests/unit/lib/ftp-tests.js
@@ -1,12 +1,34 @@
 const chai    = require("chai");
-const expect  = chai.expect;
-const ftp = require("../../../lib/ftp.js");
+const { assert, expect }  = chai;
+const { SendFile } = require("../../../lib/ftp.js");
 const Future  = require("fluture");
+const { fork, resolve, reject } = Future;
 
-describe("Unit Tests - FTP"), function() {
+
+describe("Unit Tests - FTP", function() {
     describe("SendFile", function() {
         it("should return a future", function() {
+            const fakeConnectionConfig = {
+                "host" : "",
+                "port" : "1",
+                "remoteFilePath" : "",
+                "user" : "",
+                "password" : {
+                    "obj" : "val"
+                }
+            };
 
+            const mockFtpClient = {
+                "connect" : (connectionconfiguration) => {},
+                "put" : (file, path, err) => {
+                    Future.resolve();
+                }
+            };
+
+            expect(SendFile (mockFtpClient) ("") (fakeConnectionConfig)).to.be.instanceOf(Future);
+        });
+
+        it("should resolve with a success message if put succeeds", function() {
             const fakeConnectionConfig = {
                 "host" : "",
                 "port" : 1,
@@ -14,10 +36,48 @@ describe("Unit Tests - FTP"), function() {
                 "user" : "",
                 "password" : ""
             };
-            //         host: $.String, port: $.NonNegativeInteger, remoteFilePath: $.String, user: $.String, password: $.String
 
+            const mockFtpClient = {
+                "connect" : (connectionconfiguration) => {},
+                "put" : (file, path, err) => {
+                    Future.resolve();
+                }
+            };
 
-            expect(ftp.SendFile("", fakeConnectionConfig)).to.be.instanceOf(Future);
+            fork 
+            (err => {
+                assert.fail();
+            })
+            (result => {
+                expect(result).to.equal("File sent successfully");
+            })
+            (SendFile (mockFtpClient) ("") (fakeConnectionConfig))
+        });
+
+        it("should resolve with a descriptive error if put fails", function() {
+            const fakeConnectionConfig = {
+                "host" : "",
+                "port" : 1,
+                "remoteFilePath" : "",
+                "user" : "",
+                "password" : ""
+            };
+
+            const mockFtpClient = {
+                "connect" : (connectionconfiguration) => {},
+                "put" : (file, path, err) => {
+                    err("string");
+                }
+            };
+
+            fork 
+            (err => {
+                expect(err).to.equal("put failed");
+            })
+            (result => {
+                console.log(result);
+            })
+            (SendFile (mockFtpClient) ("") (fakeConnectionConfig))
         });
     });
-}
+});

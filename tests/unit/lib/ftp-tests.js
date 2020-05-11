7b2d1722eb46400c5e5905126e9dfea26d9e3d20
Jack Goza <jgoza@q6inc.com>
2020-05-11 16:42:41 -0500

    rework sendFile method to return a valid future add the Future(rej, res) as the explicit return type for the sendFile method. rename SendFile => sendFile change tests in ftp-tests.js to use done since they are testing async functionality do minor cleaning in sanctuaryEnvironment

--- tests/unit/lib/ftp-tests.js
+++ tests/unit/lib/ftp-tests.js
@@ -1,36 +1,14 @@
 const chai    = require("chai");
 const { assert, expect }  = chai;
-const { SendFile } = require("../../../lib/ftp.js");
+const { sendFile } = require("../../../lib/ftp.js");
 const Future  = require("fluture");
 const { fork, resolve, reject } = Future;
 
-
 describe("Unit Tests - FTP", function() {
-    describe("SendFile", function() {
+    describe("sendFile", function() {
         it("should return a future", function() {
             const fakeConnectionConfig = {
                 "host" : "",
-                "port" : "1",
-                "remoteFilePath" : "",
-                "user" : "",
-                "password" : {
-                    "obj" : "val"
-                }
-            };
-
-            const mockFtpClient = {
-                "connect" : (connectionconfiguration) => {},
-                "put" : (file, path, err) => {
-                    Future.resolve();
-                }
-            };
-
-            expect(SendFile (mockFtpClient) ("") (fakeConnectionConfig)).to.be.instanceOf(Future);
-        });
-
-        it("should resolve with a success message if put succeeds", function() {
-            const fakeConnectionConfig = {
-                "host" : "",
                 "port" : 1,
                 "remoteFilePath" : "",
                 "user" : "",
@@ -44,17 +22,35 @@ describe("Unit Tests - FTP", function() {
                 }
             };
 
-            fork 
-            (err => {
-                assert.fail();
-            })
+            expect(sendFile (mockFtpClient) ("") (fakeConnectionConfig)).to.be.instanceOf(Future);
+        });
+
+        it("should resolve with a success message if put succeeds", function(done) {
+            const fakeConnectionConfig = {
+                "host" : "",
+                "port" : 1,
+                "remoteFilePath" : "",
+                "user" : "",
+                "password" : ""
+            };
+
+            const mockFtpClient = {
+                "connect" : (connectionconfiguration) => {},
+                "put" : (file, path, callback) => {
+                    callback();
+                }
+            };
+
+            fork
+            (done)
             (result => {
                 expect(result).to.equal("File sent successfully");
+                done();
             })
-            (SendFile (mockFtpClient) ("") (fakeConnectionConfig))
+            (sendFile (mockFtpClient) ("") (fakeConnectionConfig))
         });
 
-        it("should resolve with a descriptive error if put fails", function() {
+        it("should reject if file send fails", function(done) {
             const fakeConnectionConfig = {
                 "host" : "",
                 "port" : 1,
@@ -65,19 +61,18 @@ describe("Unit Tests - FTP", function() {
 
             const mockFtpClient = {
                 "connect" : (connectionconfiguration) => {},
-                "put" : (file, path, err) => {
-                    err("string");
+                "put" : (file, path, callback) => {
+                    callback("put failed");
                 }
             };
 
-            fork 
+            fork
             (err => {
-                expect(err).to.equal("put failed");
+                expect(err).to.equal("Put to FTP failed with error: put failed");
+                done();
             })
-            (result => {
-                console.log(result);
-            })
-            (SendFile (mockFtpClient) ("") (fakeConnectionConfig))
+            (done)
+            (sendFile (mockFtpClient) ("") (fakeConnectionConfig))
         });
     });
 });

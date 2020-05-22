666ef57f4d8714b22bc97a100b770dadfb0f8e74
Jack Goza <jgoza@q6inc.com>
2020-05-22 11:30:10 -0500

    add sftp system tests and static ssh keys for sftp container
    create ssh pub and private keys for ssh container usage
    change dockerfile for sftp container to refer to aformentioned keys
    add sftp system tests for create & auth failures

---
+++ tests/system/lib/sftp-tests.js
@@ -0,0 +1,104 @@
+const chai = require("chai");
+const expect = chai.expect;
+const SftpClient = require("ssh2").Client;
+const { fork } = require("fluture");
+const path = require("path");
+const Readable = require('stream').Readable;
+const { sendFileViaSftp } = require(path.join(__dirname, "../../../lib/sftp.js"));
+
+const cleanUpSftp = (connectionConfig) => {
+  const client = new SftpClient();
+
+  client.on('error', (err) => {
+    console.log(err);
+  });
+
+  client.on("ready", () => {
+    client.sftp((err, sftp) => {
+      if (err) {
+        client.end();
+        throw err;
+      }
+
+      sftp.unlink(connectionConfig.remoteFilePath, (err) => {
+        client.end();
+        throw err;
+      });
+      client.end();
+    });
+  }).connect(connectionConfig);
+}
+
+describe("SYSTEM TESTS - sftp.js", function () {
+  describe("sendFileViaSftp", function () {
+    it("should put a file on an sftp server", function (done) {
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteFilePath": "some_file.txt",
+        "user": "user",
+        "password": "password"
+      };
+
+      const readable = new Readable();
+
+      fork
+        (err => {
+          console.log("err in fork");
+          console.log(err);
+          done;
+        })
+        (data => {
+          expect(data).to.deep.equal("Upload successful");
+          done();
+        })
+        (sendFileViaSftp(new SftpClient())(readable)(connectionConfig))
+
+      readable.push("hello world");
+      readable.push(null);
+    });
+
+    it("should fail with a descritive message if there is an error", function(done) {
+      // anonymous login not allowed in test sftp server
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteFilePath": "some_file.txt",
+        "user": "",
+        "password": ""
+      };
+
+      const readable = new Readable();
+
+      fork
+        (err => {
+          expect(err.message).to.contain("All configured authentication methods failed");
+          done();
+        })
+        (done)
+        (sendFileViaSftp(new SftpClient())(readable)(connectionConfig))
+
+      readable.push("hello world");
+      readable.push(null);
+    });
+
+    after(function (done) {
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteFilePath": "some_file.txt",
+        "user": "user",
+        "password": "password"
+      };
+
+      try {
+        cleanUpSftp(connectionConfig);
+        done();
+      } catch (err) {
+        console.log("an error occurred in the after function");
+        console.log(err);
+        done;
+      }
+    });
+  });
+});

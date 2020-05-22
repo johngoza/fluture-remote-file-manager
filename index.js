7760789217dda7abc9dda5a5d1a162dea9fbbf7c
Jack Goza <jgoza@q6inc.com>
2020-05-22 14:55:46 -0500

    pin eslint to static version

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {FutureType} = require("fluture-sanctuary-types");
 const {def, FtpConnectionConfig} = require("./sanctuaryEnvironment");
 const {Future} = require("fluture");
 const FtpClient = require("ftp");
-var SftpClient = require("ssh2").Client;
+const SftpClient = require("ssh2").Client;
 const sendFileViaFtp = require("./lib/ftp");
 const sendFileViaSftp = require("./lib/sftp");
 

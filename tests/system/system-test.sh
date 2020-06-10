7dbff7737832c6b87e593872c3e0cd263d5254c9
Michael Reinig Jr <mreinigjr@q6inc.com>
2020-06-10 17:03:10 -0500

    only exiting with a 1 or 0

--- tests/system/system-test.sh
+++ tests/system/system-test.sh
@@ -4,4 +4,10 @@ cd ./tests/system/
 docker-compose up --build --abort-on-container-exit --exit-code-from test-server
 status=$?
 docker-compose down --volumes --remove-orphans --rmi all
-exit status
+
+if test $status -eq 0
+then
+  exit 0
+else
+  exit 1
+fi

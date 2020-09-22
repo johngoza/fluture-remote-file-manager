5685489db292b9b1ebbc2c2d52d8f06e395b58dc
Jack Goza <jgoza@q6inc.com>
2020-09-22 14:08:47 -0500

    Get system tests passing; handle ftp closing better
    Modified ftp system tests slightly (things like changing ftps tests to
    hit the ftps container, not the ftp one :facepalm:). Added hello.txt to
    ftps resources to fix get test. added better ftp/s connection close
    handling so system tests don't overlap.

--- tests/system/lib/ftp-tests.js
+++ tests/system/lib/ftp-tests.js
@@ -5,6 +5,8 @@ const path = require("path");
 const {fork} = require("fluture");
 const {getFile, sendFile} = require(path.join(__dirname, "../../../index"));
 
+// if you see something like ERR_SSL_WRONG_VERSION_NUMBER it's probably not an auth/ssl issue
+// chances are excellent you have a test that's not closing its connection
 describe("SYSTEM TESTS - ftp.js", function() {
   describe("getFileViaFtp", function() {
     it("should get a file on an ftp server", function(done) {
@@ -137,44 +139,42 @@ describe("SYSTEM TESTS - ftp.js", function() {
   });
 
   describe("sendFileViaFtps", function() {
-    // todo: this fails with "530 Unable to secure connection(s)". fix it
-    // it("should get a file on an ftps server", function(done) {
-    //   this.timeout(5000);
-    //
-    //   // the hello.txt file can be found in /tests/system/resources/ftp
-    //   const connectionConfig = {
-    //     "host": "ftp-server",
-    //     "port": 21,
-    //     "remoteDirectory": "/user/",
-    //     "remoteFileName": "hello.txt",
-    //     "user": "user",
-    //     "password": "password",
-    //     "secure": true,
-    //     "secureOptions": {
-    //       "rejectUnauthorized": false,
-    //       "secureProtocol": "TLSv1_method",
-    //     },
-    //   };
-    //
-    //   fork
-    //   (done)
-    //   (data => {
-    //     let result = "";
-    //
-    //     data.on("data", function(d) {
-    //       result += d.toString();
-    //     });
-    //
-    //     data.on("end", function() {
-    //       expect(result).to.deep.equal("hello world");
-    //       done();
-    //     });
-    //   })
-    //   (getFile("ftp")(connectionConfig));
-    // });
+    it("should get a file on an ftps server", function(done) {
+      this.timeout(5000);
 
-    // todo: fix this
-    // Error: done() invoked with non-Error: ERR_SSL_WRONG_VERSION_NUMBER 139663953001800:error:1408F10B:SSL routines:ssl3_get_record:wrong version number:../deps/openssl/openssl/ssl/record/ssl3_record.c:308:
+      // the hello.txt file can be found in /tests/system/resources/ftp
+      const connectionConfig = {
+        "host": "ftps-server",
+        "port": 21,
+        "remoteDirectory": "/user/",
+        "remoteFileName": "hello.txt",
+        "user": "user",
+        "password": "password",
+        "secure": true,
+        "secureOptions": {
+          "rejectUnauthorized": false,
+          "secureProtocol": "TLSv1_method",
+        },
+      };
+
+      fork
+      (done)
+      (data => {
+        let result = "";
+
+        data.on("data", function(d) {
+          result += d.toString();
+          data.destroy();
+        });
+
+        data.on("end", function() {
+          expect(result).to.deep.equal("hello world");
+          done();
+        });
+      })
+      (getFile("ftp")(connectionConfig));
+    });
+
     it("should put a file on an ftps server", function(done) {
       this.timeout(5000);
 
@@ -197,6 +197,7 @@ describe("SYSTEM TESTS - ftp.js", function() {
         const ftpClient = new Ftp();
 
         ftpClient.on("error", (err) => {
+          ftpClient.end();
           throw err;
         });
 
@@ -206,6 +207,7 @@ describe("SYSTEM TESTS - ftp.js", function() {
         ftpClient.on("ready", () => {
           ftpClient.get(connectionConfig.remoteFileName, (err, stream) => {
             if (err) {
+              ftpClient.end();
               done(err);
             } else {
               const chunks = [];
@@ -219,7 +221,7 @@ describe("SYSTEM TESTS - ftp.js", function() {
                 finalVal = chunks.join("");
 
                 expect(finalVal).to.deep.equal("hello world");
-                ftpClient.destroy();
+                ftpClient.end();
                 done();
               });
             }
--- tests/system/lib/sftp-tests.js
+++ tests/system/lib/sftp-tests.js
@@ -1,164 +1,164 @@
 // todo: uncomment all this once ftps tests are fixed
 
-// const chai = require("chai");
-// const expect = chai.expect;
-// const fs = require("fs");
-// const path = require("path");
-// const {fork} = require("fluture");
-// const {getFile, sendFile} = require(path.join(__dirname, "../../../index"));
-//
-// describe("SYSTEM TESTS - sftp.js", function() {
-//   describe("sendFileViaSftp", function() {
-//     it("should put a file on an sftp server that uses key auth", function(done) {
-//       const privateKey = fs.readFileSync("tests/system/resources/sftp/host_id_rsa");
-//
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/",
-//         "remoteFileName": "some_file.txt",
-//         "user": "user",
-//         "privateKey": privateKey,
-//       };
-//
-//       const filepath = "tests/system/resources/hello.txt";
-//
-//       fork
-//       (err => {
-//         done(err);
-//       })
-//       (data => {
-//         expect(data).to.deep.equal("Upload successful");
-//         done();
-//       })
-//       (sendFile("sftp")(connectionConfig)(filepath));
-//     });
-//
-//     it("should put a file on an sftp server that uses password auth", function(done) {
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/",
-//         "remoteFileName": "some_file.txt",
-//         "user": "user",
-//         "password": "password",
-//       };
-//
-//       const filepath = "tests/system/resources/hello.txt";
-//
-//       fork
-//       (err => {
-//         done(err);
-//       })
-//       (data => {
-//         expect(data).to.deep.equal("Upload successful");
-//         done();
-//       })
-//       (sendFile("sftp")(connectionConfig)(filepath));
-//     });
-//
-//     it("should fail with a descriptive message if there is an error", function(done) {
-//       // anonymous login not allowed in test sftp server
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/",
-//         "remoteFileName": "some_file.txt",
-//         "user": "",
-//         "password": "",
-//       };
-//
-//       const filepath = "tests/system/resources/hello.txt";
-//
-//       fork
-//       (err => {
-//         expect(err.message).to.contain("All configured authentication methods failed");
-//         done();
-//       })
-//       (done)
-//       (sendFile("sftp")(connectionConfig)(filepath));
-//     });
-//   });
-//
-//   describe("getFile", function() {
-//     it("should get a file on an sftp server that uses password auth", function(done) {
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/home/user",
-//         "remoteFileName": "hello.txt",
-//         "user": "user",
-//         "password": "password",
-//       };
-//
-//       fork
-//       (err => {
-//         done(err);
-//       })
-//       (data => {
-//         let result = "";
-//
-//         data.on("data", function(d) {
-//           result += d.toString();
-//         });
-//
-//         data.on("end", function() {
-//           expect(result).to.deep.equal("hello world");
-//           done();
-//         });
-//       })
-//       (getFile("sftp")(connectionConfig));
-//     });
-//
-//     it("should get a file on an sftp server that uses key auth", function(done) {
-//       const privateKey = fs.readFileSync("tests/system/resources/sftp/host_id_rsa");
-//
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/home/user/",
-//         "remoteFileName": "hello.txt",
-//         "user": "user",
-//         "privateKey": privateKey,
-//       };
-//
-//       fork
-//       (err => {
-//         done(err);
-//       })
-//       (data => {
-//         let result = "";
-//
-//         data.on("data", function(d) {
-//           result += d.toString();
-//         });
-//
-//         data.on("end", function() {
-//           expect(result).to.deep.equal("hello world");
-//           done();
-//         });
-//       })
-//       (getFile("sftp")(connectionConfig));
-//     });
-//
-//     it("should fail with a descriptive message if there is an error", function(done) {
-//       // anonymous login not allowed in test sftp server
-//       const connectionConfig = {
-//         "host": "sftp-server",
-//         "port": 22,
-//         "remoteDirectory": "/",
-//         "remoteFileName": "some_file.txt",
-//         "user": "",
-//         "password": "",
-//       };
-//
-//       fork
-//       (err => {
-//         expect(err.message).to.contain("All configured authentication methods failed");
-//         done();
-//       })
-//       (done)
-//       (getFile("sftp")(connectionConfig));
-//     });
-//   });
-// });
+const chai = require("chai");
+const expect = chai.expect;
+const fs = require("fs");
+const path = require("path");
+const {fork} = require("fluture");
+const {getFile, sendFile} = require(path.join(__dirname, "../../../index"));
+
+describe("SYSTEM TESTS - sftp.js", function() {
+  describe("sendFileViaSftp", function() {
+    it("should put a file on an sftp server that uses key auth", function(done) {
+      const privateKey = fs.readFileSync("tests/system/resources/sftp/host_id_rsa");
+
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/",
+        "remoteFileName": "some_file.txt",
+        "user": "user",
+        "privateKey": privateKey,
+      };
+
+      const filepath = "tests/system/resources/hello.txt";
+
+      fork
+      (err => {
+        done(err);
+      })
+      (data => {
+        expect(data).to.deep.equal("Upload successful");
+        done();
+      })
+      (sendFile("sftp")(connectionConfig)(filepath));
+    });
+
+    it("should put a file on an sftp server that uses password auth", function(done) {
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/",
+        "remoteFileName": "some_file.txt",
+        "user": "user",
+        "password": "password",
+      };
+
+      const filepath = "tests/system/resources/hello.txt";
+
+      fork
+      (err => {
+        done(err);
+      })
+      (data => {
+        expect(data).to.deep.equal("Upload successful");
+        done();
+      })
+      (sendFile("sftp")(connectionConfig)(filepath));
+    });
+
+    it("should fail with a descriptive message if there is an error", function(done) {
+      // anonymous login not allowed in test sftp server
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/",
+        "remoteFileName": "some_file.txt",
+        "user": "",
+        "password": "",
+      };
+
+      const filepath = "tests/system/resources/hello.txt";
+
+      fork
+      (err => {
+        expect(err.message).to.contain("All configured authentication methods failed");
+        done();
+      })
+      (done)
+      (sendFile("sftp")(connectionConfig)(filepath));
+    });
+  });
+
+  describe("getFile", function() {
+    it("should get a file on an sftp server that uses password auth", function(done) {
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/home/user",
+        "remoteFileName": "hello.txt",
+        "user": "user",
+        "password": "password",
+      };
+
+      fork
+      (err => {
+        done(err);
+      })
+      (data => {
+        let result = "";
+
+        data.on("data", function(d) {
+          result += d.toString();
+        });
+
+        data.on("end", function() {
+          expect(result).to.deep.equal("hello world");
+          done();
+        });
+      })
+      (getFile("sftp")(connectionConfig));
+    });
+
+    it("should get a file on an sftp server that uses key auth", function(done) {
+      const privateKey = fs.readFileSync("tests/system/resources/sftp/host_id_rsa");
+
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/home/user/",
+        "remoteFileName": "hello.txt",
+        "user": "user",
+        "privateKey": privateKey,
+      };
+
+      fork
+      (err => {
+        done(err);
+      })
+      (data => {
+        let result = "";
+
+        data.on("data", function(d) {
+          result += d.toString();
+        });
+
+        data.on("end", function() {
+          expect(result).to.deep.equal("hello world");
+          done();
+        });
+      })
+      (getFile("sftp")(connectionConfig));
+    });
+
+    it("should fail with a descriptive message if there is an error", function(done) {
+      // anonymous login not allowed in test sftp server
+      const connectionConfig = {
+        "host": "sftp-server",
+        "port": 22,
+        "remoteDirectory": "/",
+        "remoteFileName": "some_file.txt",
+        "user": "",
+        "password": "",
+      };
+
+      fork
+      (err => {
+        expect(err.message).to.contain("All configured authentication methods failed");
+        done();
+      })
+      (done)
+      (getFile("sftp")(connectionConfig));
+    });
+  });
+});
